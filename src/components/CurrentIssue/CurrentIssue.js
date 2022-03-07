aa83074663669ba43634c94fd862caf431c28cad
askhazhakyan <47585332+askhazhakyan@users.noreply.github.com>
2022-03-06 22:46:56 -0800

    Update CurrentIssue.js

--- src/components/CurrentIssue/CurrentIssue.js
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -7,27 +7,27 @@ function CurrentIssue() {
   return (
     <div className="current-issues">
       <div className='indv-issue'>
-        <p className='close-issue'> Close Issue</p>
+        <p className='close'> Close Issue</p>
         <p>Assigned <span>Username</span></p>
         <p>Priority <span>Level</span></p>
         <p>Description</p>
-        <p className='description-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
+        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
       </div>
       <hr />
       <div className='indv-issue'>
-        <p className='close-issue'> Close Issue</p>
+        <p className='close'> Close Issue</p>
         <p>Assigned <span>Username</span></p>
         <p>Priority <span>Level</span></p>
         <p>Description</p>
-        <p className='description-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
+        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
       </div>
       <hr />
       <div className='indv-issue'>
-        <p className='close-issue'> Close Issue</p>
+        <p className='close'> Close Issue</p>
         <p>Assigned <span>Username</span></p>
         <p>Priority <span>Level</span></p>
         <p>Description</p>
-        <p className='description-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices. </p>
+        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices. </p>
       </div>
       <hr />
     </div>
