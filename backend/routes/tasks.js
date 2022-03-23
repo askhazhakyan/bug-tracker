692f4c3503dd5c9de6807f9ca29c2e8241379acc
askhazhakyan <47585332+askhazhakyan@users.noreply.github.com>
2022-03-22 20:15:24 -0700

    Create tasks.js

---
+++ backend/routes/tasks.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+let Task = require('../models/task.model');
+
+router.route('/').get((req, res) => {
+  Task.find()
+    .then(tasks => res.json(tasks))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/').post((req, res) => {
+  const user = req.body.user;
+  const priority = req.body.priority;
+  const description = req.body.description;
+
+  const newTask = new Task({
+    user,
+    priority,
+    description,
+  });
+
+  newTask.save()
+  .then(() => res.json('Task added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Task.findById(req.params.id)
+    .then(task => res.json(task))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Task.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Task deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').post((req, res) => {
+  Task.findById(req.params.id)
+    .then(task => {
+      task.user = req.body.user;
+      task.priority = req.body.priority;
+      task.description = req.body.description;
+
+      task.save()
+        .then(() => res.json('Task updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
