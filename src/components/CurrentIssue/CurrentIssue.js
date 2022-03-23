3424a5ce78882e2695b75f0dc9a29430d9914857
askhazhakyan <47585332+askhazhakyan@users.noreply.github.com>
2022-03-22 20:09:23 -0700

    Add files via upload

--- src/components/CurrentIssue/CurrentIssue.js
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 import './CurrentIssue.css'
 
-function CurrentIssue() {
+const Task = props => (
+  <div className='indv-issue'>
+            <p className='close'  onClick={() => { props.deleteTask(props.task._id)}}> Close Issue</p>
+            <p>Assigned <span>{props.task.user}</span></p>
+            <p>Priority <span>{props.task.priority}</span></p>
+            <p>Description</p>
+            <p className='description'>{props.task.description}</p>
+            <hr />
+          </div>
+    )
 
-  return (
-    <div className="current-issues">
-      <div className='indv-issue'>
-        <p className='close'> Close Issue</p>
-        <p>Assigned <span>Username</span></p>
-        <p>Priority <span>Level</span></p>
-        <p>Description</p>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
+class CurrentIssue extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.deleteTask = this.deleteTask.bind(this)
+
+    this.state = {task: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/tasks/')
+      .then(response => {
+        this.setState({ task: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteTask(id) {
+    axios.delete('http://localhost:5000/tasks/'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      task: this.state.task.filter(el => el._id !== id)
+    })
+  }
+
+  taskList() {
+    return this.state.task.map(currenttask => {
+      return <Task task={currenttask} deleteTask={this.deleteTask}  key={currenttask._id}/>;
+    })
+  }
+
+  render(){
+    return (
+      <div className="current-issues">
+         { this.taskList() }
       </div>
-      <hr />
-      <div className='indv-issue'>
-        <p className='close'> Close Issue</p>
-        <p>Assigned <span>Username</span></p>
-        <p>Priority <span>Level</span></p>
-        <p>Description</p>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices.</p>
-      </div>
-      <hr />
-      <div className='indv-issue'>
-        <p className='close'> Close Issue</p>
-        <p>Assigned <span>Username</span></p>
-        <p>Priority <span>Level</span></p>
-        <p>Description</p>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus odio sollicitudin efficitur mollis. Praesent pretium molestie mauris a suscipit. Cras ut porta mi. Morbi rutrum, lectus eu rhoncus tincidunt, erat dui rutrum nunc, ut congue erat risus non ipsum. Cras condimentum cursus nisl. Pellentesque dignissim posuere tortor facilisis ultrices. </p>
-      </div>
-      <hr />
-    </div>
-  );
+    );
+  }
 }
 
 export default CurrentIssue;
